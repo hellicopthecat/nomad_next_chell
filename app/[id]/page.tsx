ff6cb4cdfd324652b3447986c9241e5fbd79be14
Hellicat <jhccop@naver.com>
2024-09-19 17:40:00 +0900

    chellenge

---
+++ app/[id]/page.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import style from "./person.module.css";
+interface IPersonParams {
+  id: string;
+}
+interface IPersonType {
+  id: string;
+  state: string;
+  city: string;
+  name: string;
+  country: string;
+  position: string;
+  industries: string[];
+  financialAssets: [
+    {
+      exchange: string;
+      ticker: string;
+      companyName: string;
+      numberOfShares: number;
+      exerciseOptionPrice: number;
+      sharePrice: number;
+      currencyCode: string;
+      exchangeRate: number;
+      interactive: boolean;
+      currentPrice: number;
+    }
+  ];
+  squareImage: string;
+  bio: string[];
+  about: string[];
+  netWorth: number;
+}
+export default async function Person({params}: {params: IPersonParams}) {
+  const data: IPersonType = await (
+    await fetch(
+      `https://billions-api.nomadcoders.workers.dev/person/${params.id}`
+    )
+  ).json();
+  console.log(data);
+  return (
+    <div className={style.personCont}>
+      <section>
+        <div>
+          <Image src={data.squareImage} alt={data.id} fill={true} />
+        </div>
+        <h3>{data.name}</h3>
+        <h5>Networth : {`${data.netWorth} Billion`}</h5>
+        <h5>Country : {data.country}</h5>
+        <h5>Industry : {data.industries}</h5>
+        <p>{data.bio}</p>
+      </section>
+
+      <section>
+        <h3>Financial Assets</h3>
+        <div className={style.assetGrid}>
+          {data.financialAssets.map((asset) => (
+            <div key={asset.companyName} className={style.assetItem}>
+              <h4>Company Name : {asset.companyName}</h4>
+              <h4>Ticker : {asset.ticker}</h4>
+              <h4>
+                Exercise Price :
+                {!asset.exerciseOptionPrice
+                  ? `$ 0`
+                  : `$ ${asset.exerciseOptionPrice}`}
+              </h4>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type {Metadata} from "next";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body>
+        <main
+          style={{
+            margin: "0 auto",
+            display: "flex",
+            justifyContent: "center",
+            padding: 40,
+          }}
+        >
+          {children}
+        </main>
       </body>
     </html>
   );
